4120889c7ea1d3f531eaa2375815ee24257d657d
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-04-10 15:43:44 +0530

    completed the responsive navigation bar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import {Avatar} from 'antd';
+import { Avatar } from "antd";
 
 const navlinks = [
   {
@@ -21,29 +21,34 @@ const navlinks = [
   },
 ];
 
-export default function Navigation({user}) {
-  const [menuActive,setMenuActive] = useState(false)
+export default function Navigation({ user }) {
+  const [menuActive, setMenuActive] = useState(false);
 
   return (
     <nav className="site-navigation">
-      <span className={'menu_title'}>My React Blog</span>
-      <div className={`menu_content_container${menuActive && 'active'}`}>
-      <ul>
-        {navlinks.map((link, index) => (
-          <li key={index}>
-            <Link to={link.path}>{link.title}</Link>
-          </li>
-        ))}
-      </ul>
-        <span><Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"  size={38} />
-        <span className={'menu_avatar_name'}>
-          {`${user.firstName} ${user.lastName}`}
+      <span className={"menu_title"}>My React Blog</span>
+      <div className={`menu_content_container ${menuActive && "active"}`}>
+        <ul>
+          {navlinks.map((link, index) => (
+            <li key={index}>
+              <Link to={link.path}>{link.title}</Link>
+            </li>
+          ))}
+        </ul>
+        <span className={"manu_avatar_container"}>
+          <Avatar
+            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+            size={38}
+          />
+          <span className={"menu_avatar_name"}>
+            {`${user.firstName} ${user.lastName}`}
+          </span>
         </span>
-        </span>
-
       </div>
-      <i className={'ionicons icon ion-ios-menu'} onClick={() => setMenuActive(!menuActive)} />
-
+      <i
+        className={"ionicons icon ion-ios-menu"}
+        onClick={() => setMenuActive(!menuActive)}
+      />
     </nav>
   );
 }
